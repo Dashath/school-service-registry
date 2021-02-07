c6498274bc630a8b11eb091f5caad387761059c4
dashrath <dashrath.rajput@gmail.com>
2021-02-07 12:10:36 +0530

    initial commit

---
+++ src/main/java/com/learning/ladder/schoolserviceregistry/SchoolServiceRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.learning.ladder.schoolserviceregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolServiceRegistryApplication.class, args);
+	}
+
+}
